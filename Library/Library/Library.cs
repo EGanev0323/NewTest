97183fe5644ca7efd5836f00eed66722f4c0cdb3
Emil Ganev <emkata.ganev@gmail.com>
2021-01-06 17:26:31 +0200

    Library

---
+++ Library/Library/Library.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            //return new LibraryIterator(this.books);
+            for (int i = 0; i < books.Count; i++)
+            {
+                yield return this.books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        //private class LibraryIterator : IEnumerator<Book>
+        //{
+        //    private int currentIndex;
+        //    private readonly List<Book> books;
+        //    public LibraryIterator(List<Book> books)
+        //    {
+        //        this.Reset();
+        //        this.books = books;
+        //    }
+        //    public Book Current => this.books[currentIndex];
+
+        //    object IEnumerator.Current => this.Current;
+
+        //    public void Dispose()
+        //    {
+
+        //    }
+
+        //    public bool MoveNext()
+        //    {
+        //        currentIndex++;
+        //        return currentIndex < this.books.Count;
+        //    }
+
+        //    public void Reset()
+        //    {
+        //        this.currentIndex = -1;
+        //    }
+
+        //}
+    }
+}
---
+++ Library/Library/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "Georg Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+            Book bookFour = new Book("Pod Igoto", 1894,"Вазов");
+
+            Library libraryOne = new Library(bookFour);
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var book in libraryTwo)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+            Console.WriteLine();
+            foreach (var book in libraryOne)
+            {
+                Console.WriteLine(book.Year);
+            }
+
+        }
+    }
+}
